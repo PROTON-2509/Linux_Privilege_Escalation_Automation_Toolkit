65b603801b2c6442ce804589cb1e8e81ef040fb1
Krishna <krishnatambare2509@gmail.com>
2026-03-25 22:04:15 +0530

    Update system_info.py

--- modules/system_info.py
+++ modules/system_info.py
@@ -1,1 +1,28 @@
+import platform
+import socket
+import os
+import subprocess
 
+
+def get_system_info():
+
+    print("\n[+] Collecting System Information...\n")
+
+    try:
+        ip = subprocess.check_output("hostname -I", shell=True, text=True).strip()
+    except:
+        ip = "Unable to detect"
+
+    info = {
+        "OS": platform.system(),
+        "OS Version": platform.version(),
+        "Machine": platform.machine(),
+        "Hostname": socket.gethostname(),
+        "IP Address": ip,
+        "User": os.getlogin()
+    }
+
+    for key, value in info.items():
+        print(f"{key}: {value}")
+
+    return info
