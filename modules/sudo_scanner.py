dfbdecb091f084fab6462aefc2658897bcd2f564
Krishna <krishnatambare2509@gmail.com>
2026-03-25 22:03:13 +0530

    Update sudo_scanner.py

--- modules/sudo_scanner.py
+++ modules/sudo_scanner.py
@@ -1,1 +1,22 @@
+import subprocess
 
+
+def check_sudo():
+
+    print("\n[+] Checking Sudo Permissions...\n")
+
+    try:
+
+        result = subprocess.check_output(
+            "sudo -l",
+            shell=True,
+            text=True
+        )
+
+        print(result)
+
+        return result
+
+    except Exception as e:
+        print("Error checking sudo:", e)
+        return ""
